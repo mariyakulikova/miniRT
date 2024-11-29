e5284b1deff2f6f2742db163a5a08d047a9820d4
Mariia Kulikova <mkulikov@student.42berlin.de>
2024-11-29 11:41:08 +0100

    add ft_atof

--- src/tools/2_utils.c
+++ src/tools/2_utils.c
@@ -6,7 +6,7 @@
 /*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 17:16:18 by mkulikov          #+#    #+#             */
-/*   Updated: 2024/11/27 16:37:11 by mkulikov         ###   ########.fr       */
+/*   Updated: 2024/11/29 11:40:57 by mkulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,22 @@
 bool	is_lower(char i)
 {
 	return ((i >= 'a') && (i <= 'z'));
+}
+
+float	ft_atof(char *str)
+{
+	char	*point;
+	int		len;
+	float	integer_part;
+	float	fractional_part;
+
+	integer_part = (float)ft_atoi(str);
+	point = ft_strchr(str, '.');
+	if (!point)
+		return (integer_part);
+	fractional_part = (float)ft_atoi(++point);
+	len = ft_strlen(point);
+	while (len--)
+		fractional_part /= 10;
+	return (integer_part + fractional_part);
 }
