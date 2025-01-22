ece0e6a1db89290928211b11bbf04386c429fe28
mkulikov <mkulikov@student.42berlin.de>
2025-01-22 12:41:52 +0100

    define const

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,14 +6,15 @@
 /*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 10:50:34 by alvutina          #+#    #+#             */
-/*   Updated: 2025/01/22 12:34:46 by mkulikov         ###   ########.fr       */
+/*   Updated: 2025/01/22 12:40:47 by mkulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 # define M_PI 3.14159265358979323846
-# define EPSILON FLT_EPSILON
+# define FLT_MAX 3.40282347e+38F
+# define EPSILON 1.19209290e-7F
 # include "../minilibx-linux/mlx.h"
 # include "../libft/libft.h"
 # include <math.h>
